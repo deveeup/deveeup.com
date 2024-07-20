c2768b1772503202a3ea9145d9a49009cb6e49ef
David Camacho <jossedaviid1@gmail.com>
2024-07-19 22:04:16 -0500

    feat: first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import vercel from 'vite-plugin-vercel';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vercel()],
+  plugins: [react()],
   base: "/react-frontend-dev-portfolio/",
   resolve: {
     alias: {
